cd53e513a6806295b3fdc5b4ef80b731416b4209
juniormaurison <juniorlimateixeira@gmail.com>
2018-10-08 10:58:30 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,10 +1,10 @@
 <?php
 
 
-$hostname_conexao = "localhost";
-$database_conexao = "apirest";
-$username_conexao = "root";
-$password_conexao = "";
+$hostname_conexao = "sql10.freemysqlhosting.net";
+$database_conexao = "sql10260282";
+$username_conexao = "sql10260282";
+$password_conexao = "DGqE9If5wA";
 
 $mysqli = new mysqli($hostname_conexao, $username_conexao, $password_conexao, $database_conexao);
 
